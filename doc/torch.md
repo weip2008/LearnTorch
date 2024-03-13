6fc6368b08a1387365e864bd11f71fde49258cf9
jwang1122 <jwang1122@gmail.com>
2024-03-12 21:42:01 -0500

    create fashion model

--- doc/torch.md
+++ doc/torch.md
@@ -10,8 +10,18 @@ python -m venv env
 ❓ not available module
 ![](images/unavailable%20modue.png)
 📝 close VS Code, reopen, and run all code from the top.
-
 ## AI on Fashion
 ![](images/neuralNetwork4handwritingDigits.png)
-28X28=784 input, 0-9 output
+28X28=784 input, 2 modle layer,  0-9 output
+$$f_{l+1} = \sigma (w_l a_l + b_l) $$
+$w_l$: weight for layer l
+$b_l$: bias for layer l
+$sigma$: activation function
+$f_{l+1}$: l+1 function of layer l
+the purpose of modeling is find each $w_l$ and $b_l$
+
 ![sample fashion images](images/fashionSample.png)
+
+* [tensor basics](torchBasics.ipynb)
+
+* [Understand image data, and squeeze(), transpose()](src/fashion01.py)
---
+++ src/fashion01.py
@@ -0,0 +1,44 @@
+import torch
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Lambda
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+
+label_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+training_data = datasets.FashionMNIST(
+    root="../data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+    target_transform=Lambda(lambda y: torch.zeros(10, dtype=torch.float).scatter_(0, torch.tensor(y), value=1))
+)
+
+train_dataloader = DataLoader(training_data, batch_size=64)
+for x, y in train_dataloader:  #x: image; y:label
+    print(f"Shape of x [N, C, H, W]: {x.shape}")
+    print(f"Shape of y: {y.shape} {y.dtype}")
+    break
+
+img_idx = 11
+img = x[img_idx]
+print(y[img_idx])
+index = torch.argmax(y[img_idx]).item()
+print(img.shape, img.T.shape, index, label_names[index]) 
+# plt.imshow(img) #TypeError: Invalid shape (1, 28, 28) for image data
+
+# plt.imshow(img.T) # the transpose turn the image 90 degree
+
+# print(img.numpy().shape, img.numpy().squeeze().shape)
+# plt.imshow(img.numpy().squeeze()) # convert image to numpy, and squeeze it, it changes the tensor dimension to meet matplotlib requirement
+
+# print(img.numpy().transpose(1,2,0).shape) # this process make the image meet the plot requirement
+# plt.imshow(img.numpy().transpose(1,2,0)) # numpy.transpose()
+
+img1 = img.transpose(0,1).transpose(1,2) # switch color axis with row axis, switch color with column
+print(type(img1), img.shape, img1.shape)
+plt.imshow(img1)
+plt.show()
+plt.imshow(img.squeeze())
+plt.show()
