471f6da939b25757180ac4872c0e5c5176eed325
jwang1122 <jwang1122@gmail.com>
2024-05-24 11:53:27 -0500

    use labels array for classification

--- src/stock1.py
+++ src/stock1.py
@@ -5,13 +5,14 @@ from torchvision.transforms import ToTensor, Lambda
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 
-from stock import NeuralNetwork, file_path, getDataSet
+from stock import NeuralNetwork, file_path, getDataSet, labels
 
 model = NeuralNetwork()
-model.load_state_dict(torch.load("stock_model.pth"))
+model.load_state_dict(torch.load("stock_model_72.pth"))
 index = 0
 model.eval()
 training_data,test_data = getDataSet(file_path)
+count = 0
 for index in range(len(training_data)):
     stock, decision = training_data[index][0], training_data[index][1]
     # print(stock.shape, decision.shape, decision.ndim)
@@ -24,5 +25,8 @@ for index in range(len(training_data)):
         stock = stock.reshape(1,6,10)
         pred = model(stock)
         predicted, actual = pred[0].argmax(0), answer
-        print(f'model output: {pred[0]}')
-        print(f'Predicted: "{predicted}", Actual: "{actual}"')
+        if predicted==actual: count += 1
+        # print(f'model output: {pred[0]}')
+        print(f'Predicted: "{labels[predicted]}", Actual: "{labels[actual]}"')
+
+print(f"accuracy: {(100*count/len(training_data)):0.2f}")
