33c5c555dbd88d8d29f4a8f9f00a5d01c283a3d2
Wei <weip2008@gmail.com>
2024-06-08 15:23:39 -0500

    tvData get ES and NQ data now

--- src/tvData.py
+++ src/tvData.py
@@ -1,4 +1,5 @@
 from tvDatafeed import TvDatafeed, Interval
+from datetime import datetime
 
 username = 'YourTradingViewUsername'
 password = 'YourTradingViewPassword'
@@ -8,19 +9,41 @@ password = 'YourTradingViewPassword'
 # without user name and password
 tv = TvDatafeed()
 
-# print(tv) # <tvDatafeed.main.TvDatafeed object at 0x7fc1d6307c10>
+# add storage directory
+# import os
+# if not os.path.exists('stockdata'):
+#     os.makedirs('stockdata')
 
-# (symbol: str, exchange: str = 'NSE', interval: Interval = Interval.in_daily, n_bars: int = 10, fut_contract: int | None = None, extended_session: bool = False) -> DataFrame)
+# futures NQ and ES
+futures = ["NQ1!", "ES1!"]
+periods = [[Interval.in_1_minute, "1M"], 
+            [Interval.in_5_minute, "5M"],
+            [Interval.in_30_minute, "30M"],
+            [Interval.in_4_hour, "4H"],
+            [Interval.in_daily,"1D"]]
+# print(periods[3][1])
 
-# Test with a known working symbol
-#test_data = tv.get_hist(symbol='NDX', exchange='NASDAQ', interval=Interval.c, n_bars=5)
-#print(test_data)
+merged = [[future] + period for future in futures for period in periods]
+# print(merged)
 
-# futures continuous contract
-# nq_data = tv.get_hist(symbol='NQ', exchange='CME', interval=Interval.in_1_minute, n_bars=5, fut_contract=1)
-nq_data = tv.get_hist(symbol='NQ1!', exchange='CME_MINI', interval=Interval.in_1_minute, n_bars=100000)
-print(nq_data)
+for sublist in merged:
+    today = datetime.now().strftime("%Y-%m-%d")
+    csv_file_name = f'{sublist[0]}_{today}_{sublist[2]}.csv'
+    #print(csv_file_name)
+    sublist.append(csv_file_name)
 
-# downloading data for extended market hours
-# extended_price_data = tv.get_hist(symbol="EICHERMOT",exchange="NSE",interval=Interval.in_1_hour,n_bars=500, extended_session=False)
-# print(extended_price_data)
+#print(merged)
+
+# nq_data = tv.get_hist(symbol='NQ1!', exchange='CME_MINI', interval=Interval.in_1_minute, n_bars=100000)
+# print(nq_data)
+for sublist in merged:
+    nq_data = tv.get_hist(symbol=sublist[0], exchange='CME_MINI', interval=sublist[1], n_bars=100000)
+    nq_data.to_csv(f'stockdata/{sublist[3]}')
+
+""" strings = [p[1] for p in periods]
+print(strings)
+filenames = [f'{datetime.now().strftime("%Y-%m-%d_")}' +  s for s in strings]
+csvfilenames = [s + ".csv" for s in filenames]
+print(csvfilenames) """
+
+# nq_data.to_csv("nq_1m.csv")
