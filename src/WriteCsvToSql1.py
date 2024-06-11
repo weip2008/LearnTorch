b5e45b79b48f971554f2a9b4b36e258d5c7ce339
haozhou2019 <hao_zhou@yahoo.com>
2024-06-11 14:37:44 -0500

    1) Change data file format; 2) Upload those data into BigData database

--- src/WriteCsvToSql1.py
+++ src/WriteCsvToSql1.py
@@ -1,9 +1,8 @@
-import sqlite3
-import pandas as pd
-import time
 import os
-
-
+import time
+import pandas as pd
+import sqlite3
+from datetime import datetime
 
 def measure_operation_time(operation_func, *args, **kwargs):
     """
@@ -25,26 +24,27 @@ def measure_operation_time(operation_func, *args, **kwargs):
 
 
 def read_CSV_file(csv_file_path):
-    # Read data from the CSV file into a DataFrame
-    df = pd.read_csv(csv_file_path)
+    # Read data from the CSV file into a DataFrame with specified column names
+    df = pd.read_csv(csv_file_path, header=None, names=["Datetime", "Open", "High", "Low", "Close", "Volume"])
+    
+    # Convert the 'Datetime' column to the desired format
+    df['Datetime'] = df['Datetime'].apply(lambda x: datetime.strptime(x, "%Y%m%d %H%M%S").strftime("%Y-%m-%d %H:%M:%S"))
+    
     return df
 
 
 # Define the path to the CSV file
-csv_file_path = os.path.join("stockdata", "DAT_ASCII_SPXUSD_M1_2010.csv")
-#csv_file_path = "stockdata\\SPY_2024-04-11_2024-05-26_1m.csv"
+#csv_file_path = os.path.join("stockdata", "DAT_ASCII_SPXUSD_M1_2010.csv")
+#csv_file_path = os.path.join("stockdata", "DAT_ASCII_SPXUSD_M1_2011.csv")
+csv_file_path = os.path.join("stockdata", "DAT_ASCII_SPXUSD_M1_2012.csv")
 
 # Define the table name as a string variable
 table_name = "SPY_1m"
 
-# Read data from the CSV file into a DataFrame
-#ohlcv = pd.read_csv(csv_file_path)
-# Corrected usage: pass function object and its arguments separately
+# Measure the time to read data from the CSV file into a DataFrame
 time_elapsed, ohlcv = measure_operation_time(read_CSV_file, csv_file_path)
 print("Time elapsed:", time_elapsed, "seconds")
 
-# Ensure the 'Datetime' column is in ISO 8601 format with timezone information
-#ohlcv['Datetime'] = pd.to_datetime(ohlcv['Datetime']).dt.strftime('%Y-%m-%d %H:%M:%S%z')
 print(ohlcv)
 print("\n\n=============================1===Create/Insert=======================\n\n")
 
@@ -68,78 +68,27 @@ CREATE TABLE IF NOT EXISTS {table_name} (
 '''
 cursor.execute(create_table_query)
 
-# Insert data into the table
-# for index, row in ohlcv.iterrows():
-#     cursor.execute('''
-#         INSERT OR REPLACE INTO AAPL_1m (Datetime, Open, High, Low, Close, AdjClose, Volume)
-#         VALUES (?, ?, ?, ?, ?, ?, ?)
-#     ''', (row['Datetime'], row['Open'], row['High'], row['Low'], row['Close'], row['Adj Close'], row['Volume']))
-
 # Insert data into the table using the string variable
 for index, row in ohlcv.iterrows():
     insert_query = f'''
     INSERT OR REPLACE INTO {table_name} (Datetime, Open, High, Low, Close, Volume)
-    VALUES (?, ?, ?, ?, ?, ?, ?)
+    VALUES (?, ?, ?, ?, ?, ?)
     '''
-    cursor.execute(insert_query,(row['Datetime'], row['Open'], row['High'], row['Low'], row['Close'], row['Volume']))
+    cursor.execute(insert_query, (row['Datetime'], row['Open'], row['High'], row['Low'], row['Close'], row['Volume']))
 
 # Commit the changes
 conn.commit()
 
-# Delete the ohlcv variable to save memory
-del ohlcv
 
 print("\n\n=============================2===Query/Count=======================\n\n")
-
 
 count_query = f'''
 SELECT COUNT(*) FROM {table_name}
 '''
 cursor.execute(count_query)
 count_result = cursor.fetchone()[0]
-#print("\nNumber of records in AAPL_1m table:", count_result)
-print(f"\nNumber of records in {table_name} table:", count_result)
+print(f"Number of records in {table_name} table:", count_result)
 
-
-print("\n\n==========================3===Query==========================\n\n")
-
-
-# Query the data for May 6th, 2024, at 10 AM
-query_10am = f'''
-SELECT * FROM {table_name}
-WHERE Datetime LIKE '2024-05-06 09:5%'
-'''
-cursor.execute(query_10am)
-rows_10am = cursor.fetchall()
-
-print("Datatype of rows_10am:", type(rows_10am))
-print("Data for May 6th, 2024, at 10 AM:")
-for row in rows_10am:
-    print(row)
-
-
-print("\n\n==========================4===Query==========================\n\n")
-
-# Define the query date range
-query_start = "2024-05-06"
-query_end = "2024-05-12"
-
-# Query the data between May 6th, 2024, and May 12th, 2024
-# query_range = '''
-# SELECT * FROM AAPL_1m
-# WHERE Datetime BETWEEN ? AND ?
-# '''
-query_range = f'''
-SELECT * FROM {table_name}
-WHERE Datetime BETWEEN ? AND ?
-'''
-# Save the query result into a DataFrame object named query_result_df
-query_result_df = pd.read_sql_query(query_range, conn, params=(query_start, query_end))
-
-print("Length of query result is:", len(query_result_df))
-print("Datatype of query result:", type(query_result_df))
-print(query_result_df)
 
 # Close the connection
 conn.close()
-
