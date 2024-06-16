61051240f1bd406827b81153cfe782e688afeac6
jwang1122 <jwang1122@gmail.com>
2024-06-16 10:05:19 -0500

    optimize dotProduct.py

--- src/dotProduct.py
+++ src/dotProduct.py
@@ -3,11 +3,14 @@ import torch
 a = torch.tensor([1,4,6])
 print(a.shape)
 a = a.reshape(1,3)
-print(f'Matrix A shape: {a.shape}')
+print(f'{a.shape}', end=' ')
 
 b = torch.tensor([[2,3],[5,8],[7,9]])
-print(f'Matrix B shape: {b.shape}')
+print(f'{b.shape}')
 
 c =a @ b
-print(c.shape)
+# c =b @ a
+print(c.shape) # a=(1,3); b=(3,2); c⟹(1,2)
+d = c.squeeze()
+print(d.shape)
 print(c)
