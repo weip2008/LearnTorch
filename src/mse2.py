f3cff8554201022deb5a4c978b90a569530abdc8
jwang1122 <jwang1122@gmail.com>
2024-06-20 15:14:25 -0500

    understand better on w,b adjust

--- src/mse2.py
+++ src/mse2.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 # Generate some random data
 np.random.seed(42)
 X = 2 * np.random.rand(100, 1)
+y = 4 + 3*X
 y1 = 4 + 3 * X + np.random.randn(100, 1)*0.2
 y2 = 4.3 + 3.5 * X + np.random.randn(100, 1)*0.2
 y3 = 3.5 + 2.5 * X + np.random.randn(100, 1)*0.2
@@ -11,6 +12,7 @@ y3 = 3.5 + 2.5 * X + np.random.randn(100, 1)*0.2
 # Plot the data
 plt.figure(figsize=(8, 6))
 # plt.plot(X, y, c='blue', marker='o', label='Data points')
+plt.plot(X, y, 'k-', label='y=3x+4')
 plt.scatter(X, y1, c='blue', marker='o', label='y1=3x+4')
 plt.scatter(X, y2, c='red', marker='^', label='y2=3.5x+4.3')
 plt.scatter(X, y3, c='black', marker='*', label='y3=2.5x+3.5')
